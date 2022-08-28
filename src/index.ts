d7879a8b8383363c975d469326e279e1f20fc513
Renat Gubaev <renat.gubaev@gmail.com>
2022-08-28 11:32:34 +0300

    auto commit

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './ITransportFabricCommandOptions';
-export * from './ITransportFabricRequestOptions';
-export * from './ITransportFabricRequestPayload';
-export * from './ITransportFabricResponsePayload';
-export * from './TransportFabricCommandOptions';
-export * from './TransportFabricRequestPayload';
-export * from './TransportFabricResponsePayload';
-//
-export * from './constants';
